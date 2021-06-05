851137fa055cee2e2ed399fa5763c0fa600cd31b
iths-lisa-beverborg <54471572+LisaBeverborg@users.noreply.github.com>
2021-06-05 15:06:21 +0200

    style contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -14,10 +14,17 @@ import { Component, OnInit } from '@angular/core';
     <label for="name" class="label-name">
       <span class="content-name">Name</span>
     </label>
-    </div>      
-    <input type="email" name="email" autocomplete="off" required>
-    <textarea name="message" rows="10" autocomplete="off" required></textarea>
-    <button type="submit">Send</button>
+    </div>   
+    <div class="form-name"> 
+    <input type="email" name="name" autocomplete="off" required>
+    <label for="name" class="label-name">
+      <span class="content-name">Email</span>
+      </label>
+    </div>
+    <div class="form-textarea"> 
+      <textarea style="margin-top: 1rem; width:100%" name="message" rows="10" autocomplete="off" placeholder="Your Message" required></textarea>
+    </div>
+    <button class="animated-button" type="submit">Send</button>
   </form>
 
   </section>
@@ -35,7 +42,85 @@ import { Component, OnInit } from '@angular/core';
   </section>
   </div>
   </div>`,
-  styles: []
+  styles: [
+    `.image > h1:first-of-type {
+      margin-bottom: 0;
+    }
+    
+    .contact-icons {
+      display: flex;
+      justify-content: center;
+    }
+    
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    
+    .form-name{
+      width: 70%;
+      height: 50px;
+      position: relative;
+      overflow: hidden;
+    }
+    
+    .form-name input {
+      width: 100%;
+      height: 100%;
+      padding-top: 20px;
+      border: none;
+      outline: none;
+      background-color: #ececec;
+      font-size: 20px;
+    }
+    
+    .form-name label {
+      position: absolute;
+      bottom: 0px;
+      left: 0%;
+      height: 100%;
+      width: 100%;
+      pointer-events: none;
+      border-bottom: 1px solid black;
+    }
+    
+    .form-name label::after {
+      content: "";
+      position: absolute;
+      left: -1px;
+      bottom: 0px;
+      height: 100%;
+      width: 100%;
+      border-bottom: 2px solid green;
+      transform: translate(-100%);
+      transition: transform 0.3s ease;
+    }
+    
+    .content-name {
+      position: absolute;
+      bottom: 5px;
+      left: 0px;
+      transition: all 0.3s ease;
+    }
+    
+    .form-name input:focus + .label-name .content-name,
+    .form-name input:valid + .label-name .content-name {
+      transform: translateY(-150%);
+      font-size: 16px;
+      color: green;
+    }
+    
+    .form-name input:focus + .label-name::after {
+      transform: translateX(0%);
+    }
+    
+    .form-textarea {
+      width: 70%;
+      padding: 5px;
+      font-size: 16px;
+    }`
+  ]
 })
 export class ContactComponent implements OnInit {
   constructor() { }
